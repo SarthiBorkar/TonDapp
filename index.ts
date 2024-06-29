b7a2180cc5ec64d522339ce82b646f6dc619143e
Sarthi Borkar <sarthiborkar7@gmail.com>
2024-06-29 05:50:17 +0000

    ton nft testnet

--- index.ts
+++ index.ts
@@ -1,2 +1,13 @@
+import {Address, TonClient} from "ton"
+import {getHttpEndpoint} from "@orbs-network/ton-access";
+async function main () {
+    const wallet = Address.parse("UQAGdoCWkMM2xAHy6YtFLDxuBUN91GyGq1LfCumXNs5hZKVf")
+    const collection = Address.parse("EQDk8N7xM5D669LC2YACrseBJtDyFqwtSPCNhRWXU7kjEptX")
 
-// write your NFT miner here
+    const endpoint = await gethttpEndpoint({
+        network: "testnet,"
+    });
+    const miningData = await client.callGetmethod(collection, 'get_mining_data')
+    console.log(miningData)
+}
+main()
